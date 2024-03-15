eb8ffb8b83d1ed1e7e30da61cfa4ec44aae6fa33
vikas9872 <shanabhogvikas648.com>
2024-03-15 22:03:32 +0530

    001_Return Statements in React JS

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // 1. FIrst way: For returning single statement
+  // return <h1>This is the First way to return the single statement</h1>
+
+
+  // 2. Second way: For returning single statement
+  // return (
+  //   <h1>This is the Second way to return the single statement</h1>
+  // )
+
+  // 3. First way: For returning the multiple statements
+  // return (
+  //   <div>
+  //     <h1>This is the First way to return the multiple statements</h1>
+  //     <h2>Say Cheese.....</h2>
+  //   </div>
+  // )
+
+  // 4. Second way: For returning the multiple statements
+  return (
+    <>
+      <h1>This is the Second way to return the multiple statements</h1>
+      <h2>Say Cheese.....</h2>
+    </>
+  )
+
+
+}
+
+export default App;
